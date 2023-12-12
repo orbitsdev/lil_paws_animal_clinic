565cc31f7ebc40bc61cbecf0a0ca079f09de7aea
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-12-12 09:14:11 +0800

    wip

--- app/Policies/AnimalPolicy.php
+++ app/Policies/AnimalPolicy.php
@@ -12,10 +12,10 @@ class AnimalPolicy
     // /**
     //  * Determine whether the user can view any models.
     //  */
-    // public function viewAny(User $user): bool
-    // {
-    //     //
-    // }
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
 
     // /**
     //  * Determine whether the user can view the model.
